bfd0168a158f6fabd129ee9298311d2ee8860a44
NDng25 <ninhduong25nd@gmail.com>
2021-08-13 11:05:12 +0700

    Add project files.

---
+++ Models/DAO/CategoryDAO.cs
@@ -0,0 +1,22 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DAO
+{
+    public class CategoryDAO
+    {
+        FreeCyDB db = new FreeCyDB();
+        public List<Product> ListAllCategoty(int id_product)
+        {
+            return db.Products.Where(x => x.Status == 1 && x.ID_Category == id_product).ToList();
+        }
+        public Category ViewDetail(int cateId)
+        {
+            return db.Categories.Find(cateId);
+        }
+    }
+}
